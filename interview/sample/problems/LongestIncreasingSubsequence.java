b73668b10fc5115c9a866c87f545424ea7ddf4ce
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2025-04-23 23:35:32 +0200

    Create LongestIncreasingSubsequence.java

---
+++ interview/sample/problems/LongestIncreasingSubsequence.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        // Initialize DP array
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        // Find the maximum value in dp array
+        int max = 0;
+        for (int value : dp) {
+            max = Math.max(max, value);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println("Length of LIS: " + lis.lengthOfLIS(nums));
+    }
+}
