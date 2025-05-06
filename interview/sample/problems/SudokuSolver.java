3aca45fbac0a5e398f3d65ff418535aefddf9087
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2025-05-06 22:27:08 +0200

    Create SudokuSolver.java

---
+++ interview/sample/problems/SudokuSolver.java
@@ -0,0 +1,71 @@
+public class SudokuSolver {
+    public void solveSudoku(char[][] board) {
+        backtrack(board);
+    }
+
+    private boolean backtrack(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') { // Empty cell
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (backtrack(board)) {
+                                return true;
+                            }
+                            board[row][col] = '.'; // Backtrack
+                        }
+                    }
+                    return false; // Trigger backtracking
+                }
+            }
+        }
+        return true; // Solved
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        // Check the row
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) return false;
+        }
+
+        // Check the column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) return false;
+        }
+
+        // Check the 3x3 subgrid
+        int startRow = 3 * (row / 3);
+        int startCol = 3 * (col / 3);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[startRow + i][startCol + j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        char[][] sudokuBoard = {
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        SudokuSolver solver = new SudokuSolver();
+        solver.solveSudoku(sudokuBoard);
+
+        for (char[] row : sudokuBoard) {
+            System.out.println(new String(row));
+        }
+    }
+}
