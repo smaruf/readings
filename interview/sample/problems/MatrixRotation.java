b2e130414c2e6ec879f9729f1943756587ee5802
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2025-07-02 20:58:06 +0600

    Create MatrixRotation.java

---
+++ interview/sample/problems/MatrixRotation.java
@@ -0,0 +1,52 @@
+public class MatrixRotation {
+
+    public static void rotate90Clockwise(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        rotate90Clockwise(matrix);
+
+        System.out.println("Rotated Matrix (90 degrees clockwise):");
+        printMatrix(matrix);
+    }
+}
