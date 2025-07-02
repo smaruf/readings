373f5fef3d7ef0775b1eb0e4c16c297fe2fbaa66
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2025-07-02 21:04:46 +0600

    Create MatrixRotationCounterClock.java

---
+++ interview/sample/problems/MatrixRotationCounterClock.java
@@ -0,0 +1,52 @@
+public class MatrixRotationCounterClock {
+
+    public static void rotate90CounterClockwise(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each column
+        for (int j = 0; j < n; j++) {
+            int top = 0, bottom = n - 1;
+            while (top < bottom) {
+                int temp = matrix[top][j];
+                matrix[top][j] = matrix[bottom][j];
+                matrix[bottom][j] = temp;
+                top++;
+                bottom--;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        rotate90CounterClockwise(matrix);
+
+        System.out.println("Rotated Matrix (90 degrees counter-clockwise):");
+        printMatrix(matrix);
+    }
+}
